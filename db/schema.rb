c94478fd8cac6cfd804fbe58f0cd08cb6e1fecfa
francksefu <francksefu1998@gmail.com>
2024-01-21 03:11:02 +0200

    create database and run migration

---
+++ db/schema.rb
@@ -0,0 +1,168 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_01_21_010608) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "accountants", force: :cascade do |t|
+    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "classrooms", force: :cascade do |t|
+    t.string "section"
+    t.string "name"
+    t.string "classement"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "course_classroom_teachers", force: :cascade do |t|
+    t.bigint "course_id"
+    t.bigint "classroom_id"
+    t.bigint "teacher_id"
+    t.bigint "year_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["classroom_id"], name: "index_course_classroom_teachers_on_classroom_id"
+    t.index ["course_id"], name: "index_course_classroom_teachers_on_course_id"
+    t.index ["teacher_id"], name: "index_course_classroom_teachers_on_teacher_id"
+    t.index ["year_id"], name: "index_course_classroom_teachers_on_year_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.float "credit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "detail_paiement_classrooms", force: :cascade do |t|
+    t.bigint "classroom_id"
+    t.bigint "detail_paiement_id"
+    t.bigint "year_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["classroom_id"], name: "index_detail_paiement_classrooms_on_classroom_id"
+    t.index ["detail_paiement_id"], name: "index_detail_paiement_classrooms_on_detail_paiement_id"
+    t.index ["year_id"], name: "index_detail_paiement_classrooms_on_year_id"
+  end
+
+  create_table "detail_paiements", force: :cascade do |t|
+    t.string "frais_name"
+    t.float "montant"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "evaluations", force: :cascade do |t|
+    t.float "point"
+    t.float "maximum"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "information", force: :cascade do |t|
+    t.string "source"
+    t.string "destination"
+    t.text "contenu"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "paiements", force: :cascade do |t|
+    t.date "date"
+    t.float "montant"
+    t.float "reste"
+    t.bigint "promotion_id"
+    t.bigint "accountant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["accountant_id"], name: "index_paiements_on_accountant_id"
+    t.index ["promotion_id"], name: "index_paiements_on_promotion_id"
+  end
+
+  create_table "parents", force: :cascade do |t|
+    t.string "name"
+    t.string "first_name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "periodes", force: :cascade do |t|
+    t.string "name"
+    t.integer "during_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "promotions", force: :cascade do |t|
+    t.bigint "year_id"
+    t.bigint "classroom_id"
+    t.bigint "student_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["classroom_id"], name: "index_promotions_on_classroom_id"
+    t.index ["student_id"], name: "index_promotions_on_student_id"
+    t.index ["year_id"], name: "index_promotions_on_year_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.string "phone"
+    t.bigint "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id"], name: "index_students_on_parent_id"
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "years", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "course_classroom_teachers", "classrooms"
+  add_foreign_key "course_classroom_teachers", "courses"
+  add_foreign_key "course_classroom_teachers", "teachers"
+  add_foreign_key "course_classroom_teachers", "years"
+  add_foreign_key "detail_paiement_classrooms", "classrooms"
+  add_foreign_key "detail_paiement_classrooms", "detail_paiements"
+  add_foreign_key "detail_paiement_classrooms", "years"
+  add_foreign_key "paiements", "accountants"
+  add_foreign_key "paiements", "promotions"
+  add_foreign_key "promotions", "classrooms"
+  add_foreign_key "promotions", "students"
+  add_foreign_key "promotions", "years"
+  add_foreign_key "students", "parents"
+end
